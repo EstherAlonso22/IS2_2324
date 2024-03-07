5aeb8e457351f0c48a27dc5ba6a923a639527fa9
pmf586 <pmf586@alumnos.unican.es>
2024-03-07 13:24:16 +0100

    Completado GestionTiendas

--- Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/franquiciasUCBusiness/GestionTiendas.java
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/franquiciasUCBusiness/GestionTiendas.java
@@ -6,29 +6,57 @@ import es.unican.is2.franquiciasUCCommon.clases.Tienda;
 import es.unican.is2.franquiciasUCCommon.interfaces.*;
 
 public class GestionTiendas implements IGestionTiendas {
-
+	private ITiendasDAO tiendas; 
 	public GestionTiendas(ITiendasDAO tiendasDAO) {
-		
+		tiendas = tiendasDAO;
 	}
 
 	@Override
 	public Tienda nuevaTienda(Tienda t) throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+		// Busco si hay alguna tienda registrada con el mismo nombre en la interfaz
+		Tienda tiendaBuscada = tienda(t.getNombre());
+		
+		// Si no hay tienda con ese nombre, la creo
+		if (tiendaBuscada != null) {
+			return null;
+		}
+		
+		// Anhado tienda
+		tiendas.crearTienda(t);
+		return t;
 	}
 
 	@Override
 	public Tienda eliminarTienda(String nombre) throws OperacionNoValidaException, DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+		// Busco si hay alguna tienda registrada con el mismo nombre en la interfaz
+		Tienda tiendaBuscada = tienda(nombre);
+		
+		// Si no hay tienda con ese nombre, devuelvo null
+		if (tiendaBuscada == null) {
+			return null;
+		}
+		
+		// Si hay tienda con ese nombre pero tiene empleados, lanzo excepción
+		if (!tiendaBuscada.getEmpleados().isEmpty()) {
+			throw new OperacionNoValidaException("La tienda tiene empleados");
+		}
+		
+		// Elimino la tienda
+		tiendas.eliminarTienda(tiendaBuscada.getId());
+		return tiendaBuscada;
 	}
 
 	@Override
 	public Tienda tienda(String nombre) throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+		// Obtener tienda por el nombre
+		Tienda tienda = tiendas.tiendaPorNombre(nombre);
+		
+		// Si no hay tienda con ese nombre, devuelvo null
+		if (tienda == null) {
+			return null;
+		}
+		return tienda;
 	}
-	
 	
 
 }
