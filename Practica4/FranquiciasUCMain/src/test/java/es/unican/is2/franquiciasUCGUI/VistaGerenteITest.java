57642c2fa995521a587cd0cf238953ca1aaa7d1f
EstherAlonso22 <esther03alo@gmail.com>
2024-03-27 20:10:18 +0100

    Modificada clase VistaGestionITest

--- Practica4/FranquiciasUCMain/src/test/java/es/unican/is2/franquiciasUCGUI/VistaGerenteITest.java
+++ Practica4/FranquiciasUCMain/src/test/java/es/unican/is2/franquiciasUCGUI/VistaGerenteITest.java
@@ -1,4 +1,4 @@
-package es.unican.is2.franquiciasUCMain;
+package es.unican.is2.franquiciasUCGUI;
 
 import org.fest.swing.fixture.FrameFixture;
 import org.junit.jupiter.api.*;
@@ -7,25 +7,23 @@ import es.unican.is2.franquiciasUCBusiness.GestionEmpleados;
 import es.unican.is2.franquiciasUCBusiness.GestionTiendas;
 import es.unican.is2.franquiciasUCDAO.EmpleadosDAO;
 import es.unican.is2.franquiciasUCDAO.TiendasDAO;
-import es.unican.is2.franquiciasUCGUI.VistaGerente;
 
 
 class VistaGerenteITest {
-	private VistaGerente vista;
 	private FrameFixture demo;
-	
+
 	private TiendasDAO tiendasDAO = new TiendasDAO();
 	private EmpleadosDAO empleadosDAO = new EmpleadosDAO();
 	private GestionTiendas gTiendas = new GestionTiendas(tiendasDAO);
 	private GestionEmpleados gEmpleados = new GestionEmpleados(tiendasDAO, empleadosDAO);
-	
+
 	@BeforeEach
 	public void setUp(){
-		vista = new VistaGerente(gTiendas, gEmpleados);
+		VistaGerente vista = new VistaGerente(gTiendas, gEmpleados);
 		demo = new FrameFixture(vista);
 		vista.setVisible(true);
 	}
-	
+
 	@AfterEach
 	public void tearDown() {
 		demo.cleanUp();
@@ -35,19 +33,29 @@ class VistaGerenteITest {
 	public void test() {
 		//Comprobar que la interfaz tiene el aspecto deseado
 		demo.button("btnBuscar").requireText("Buscar");
-		
+
 		//Casos no válidos
 		demo.textBox("txtNombreTienda").enterText("Tienda1");
-		demo.button("btnPulsar").click();
+		demo.button("btnBuscar").click();
 		demo.textBox("txtDireccionTienda").requireText("Tienda no existe");
-		
+
 		demo.textBox("txtNombreTienda").enterText("");
-		demo.button("btnPulsar").click();
+		demo.button("btnBuscar").click();
 		demo.textBox("txtDireccionTienda").requireText("Tienda no existe");
-		
+
 		//Casos válidos
+		demo.textBox("txtNombreTienda").enterText("Tienda A");
+		demo.button("btnBuscar").click();
+		demo.textBox("txtDireccionTienda").requireText("Direccion A");
+		demo.textBox("txtTotalSueldos").requireText("2100");
 		
-		
+		// Sleep para visualizar como se realiza el test
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
