176aa826c8664e20422b711b2305d1f54aa8ef87
EstherAlonso22 <esther03alo@gmail.com>
2024-04-12 08:18:57 +0200

    Creadas listas

--- Practica4b/ListaOrdenadaAcotada/src/test/java/es/unican/is2/ListaOrdenadaAcotada/TestListaOrdenadaAcotada.java
+++ Practica4b/ListaOrdenadaAcotada/src/test/java/es/unican/is2/ListaOrdenadaAcotada/TestListaOrdenadaAcotada.java
@@ -8,24 +8,30 @@ import org.junit.jupiter.api.Test;
 import es.unican.is2.listaOrdenadaAcotada.ListaOrdenadaAcotada;
 
 class TestListaOrdenadaAcotada {
+	public ListaOrdenadaAcotada<Integer> lista1;
+	public ListaOrdenadaAcotada<Integer> lista2;
+	public ListaOrdenadaAcotada<Integer> lista3;
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		ListaOrdenadaAcotada<Integer> lista1 = new ListaOrdenadaAcotada<Integer> (5);
-		ListaOrdenadaAcotada<Integer> lista2 = new ListaOrdenadaAcotada<Integer> (5);
-		ListaOrdenadaAcotada<Integer> lista3 = new ListaOrdenadaAcotada<Integer> (5);
+		lista1 = new ListaOrdenadaAcotada<Integer> (5);
+		lista2 = new ListaOrdenadaAcotada<Integer> (5);
+		lista3 = new ListaOrdenadaAcotada<Integer> (5);
 		
 		//si falla el add fallan todos los demas
+		//Lista1 = [1]
 		lista1.add(1);
+		//Lista2 = [1,4,5,6]
 		lista2.add(1);
 		lista2.add(4);
 		lista2.add(5);
 		lista2.add(6);
+		//Lista3 = []
 	}
 	
 	@Test
 	void getTest() {
-		
+		lista1.get(0);
 	}
 
 	
