3e0cb65254aca7bd4ae477707617de4a1dabfc12
EstherAlonso22 <esther03alo@gmail.com>
2024-04-12 08:27:13 +0200

    Modificadas listas

--- Practica4b/ListaOrdenadaAcotada/src/test/java/es/unican/is2/ListaOrdenadaAcotada/TestListaOrdenadaAcotada.java
+++ Practica4b/ListaOrdenadaAcotada/src/test/java/es/unican/is2/ListaOrdenadaAcotada/TestListaOrdenadaAcotada.java
@@ -11,13 +11,15 @@ class TestListaOrdenadaAcotada {
 	public ListaOrdenadaAcotada<Integer> lista1;
 	public ListaOrdenadaAcotada<Integer> lista2;
 	public ListaOrdenadaAcotada<Integer> lista3;
+	public ListaOrdenadaAcotada<Integer> lista4;
 
 	@BeforeEach
 	public void setUp() throws Exception {
 		lista1 = new ListaOrdenadaAcotada<Integer> (5);
 		lista2 = new ListaOrdenadaAcotada<Integer> (5);
 		lista3 = new ListaOrdenadaAcotada<Integer> (5);
-		
+		lista4 = new ListaOrdenadaAcotada<Integer> (5);
+
 		//si falla el add fallan todos los demas
 		//Lista1 = [1]
 		lista1.add(1);
@@ -27,26 +29,41 @@ class TestListaOrdenadaAcotada {
 		lista2.add(5);
 		lista2.add(6);
 		//Lista3 = []
-	}
-	
-	@Test
-	void getTest() {
-		lista1.get(0);
+		//Lista4 = [1,4,5,6,7]
+		lista4.add(1);
+		lista4.add(4);
+		lista4.add(5);
+		lista4.add(6);
+		lista4.add(7);
 	}
 
-	
+	@Test
+	void getTest() {
+
+		//Casos válidos
+		assertDoesNotThrow(() -> {
+			assertEquals(lista1.get(0), 1);
+			assertEquals(lista2.get(1), 4);
+			assertEquals(lista2.get(3), 6);
+		});
+		
+		//Casos no válidos
+		
+	}
+
+
 	@Test
 	void testAdd() {
-		
+
 		assertDoesNotThrow(() -> {
 			//Casos validos
 			// 1º Caso
-			
-			
+
+
 		});
 
 		//Casos no válidos
-		
+
 
 	}
 }
