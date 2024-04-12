7b7e0d64628dfa273899bb8b022a45f9a9dba218
EstherAlonso22 <esther03alo@gmail.com>
2024-04-12 08:12:40 +0200

    Comenzado TestListaOrdenadaAcotada

--- Practica4b/ListaOrdenadaAcotada/src/test/java/es/unican/is2/ListaOrdenadaAcotada/TestListaOrdenadaAcotada.java
+++ Practica4b/ListaOrdenadaAcotada/src/test/java/es/unican/is2/ListaOrdenadaAcotada/TestListaOrdenadaAcotada.java
@@ -2,13 +2,30 @@ package es.unican.is2.ListaOrdenadaAcotada;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import es.unican.is2.listaOrdenadaAcotada.ListaOrdenadaAcotada;
 
 class TestListaOrdenadaAcotada {
 
+	@BeforeEach
+	public void setUp() throws Exception {
+		ListaOrdenadaAcotada<Integer> lista1 = new ListaOrdenadaAcotada<Integer> (5);
+		ListaOrdenadaAcotada<Integer> lista2 = new ListaOrdenadaAcotada<Integer> (5);
+		ListaOrdenadaAcotada<Integer> lista3 = new ListaOrdenadaAcotada<Integer> (5);
+		
+		//si falla el add fallan todos los demas
+		lista1.add(1);
+		lista2.add(1);
+		lista2.add(4);
+		lista2.add(5);
+		lista2.add(6);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getTest() {
+		
 	}
 
 }
